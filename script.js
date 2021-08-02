ba988758b95eae7cb712d603f7b63fec43815680
Matheus Bueno <matheus_bueno10@hotmail.com>
2021-08-02 14:40:04 -0300

    Funcao que adiciona as tres torrer no html

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const buttonReset = document.getElementById("buttonReset") 
-button.addEventListener("click", function () {
-	window.location.reload(true)
-      });
-      
+// const buttonReset = document.getElementById("buttonReset") 
+// button.addEventListener("click", function () {
+// 	window.location.reload(true)
+//       });
+
+function creatTower(number){
+	let board = document.getElementById("board")
+	let newTower = document.createElement("div")
+	newTower.classList.add("tower" + number)
+	board.appendChild(newTower)
+}
+
